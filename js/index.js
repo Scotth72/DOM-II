0d7e17eb8a33823b0524d5ea8b7c9447cacb29d1
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-03-10 18:12:25 -0500

    Working on RPoject DOMII

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+document.querySelectorAll(`nav a`).forEach(item => {
+    item.addEventListener(`mouseenter`, () => {
+        item.style.border = `2px dotted red`
+    })
+    item.addEventListener(`mouseleave`,() => {
+        item.style.border = `none`
+    })
+})
+
+document.querySelector(`nav`).addEventListener(`click`,() => {
+    console.log(`the nav got clicked`);
+})
+//keydown
+//const mainNav = document.querySelector(`.main-navigation`).addEventListener(`keydown`, event) {
+
+//}
+
+//wheel
+//document.querySelector(`logo-heading`).
+
+//drag / drop
+
+//load
+
+//focus
+
+//resize
+
+const windowSize = document.querySelector(`main-navigation`)
+window.addEventListener(`resize`, () => {
+    windowSize.textContent = ``
+})
+//scroll
+
+//select
+
+//dblclick
+
+
+
